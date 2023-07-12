1590c2e8d8d50c6ecde88b5b61e5b48434b15957
Gabriel Locas <galoc3@ulaval.ca>
2023-07-12 09:34:19 -0400

    Changer vitesse du PWm dintensite lumineuse a x1000

--- Core/Inc/comm.h
+++ Core/Inc/comm.h
@@ -13,7 +13,7 @@
 #define PACKET_SIZE 					8		// UART payload size
 #define TIMER_PRESCALER 				4000	// Prescaler for timers
 #define INT_MAX 						255		// Maximum size for uint8_t
-#define LIGHT_INTENSITY_PWM_MULTIPLIER 	100		// Number of times the light intensity PWM
+#define LIGHT_INTENSITY_PWM_MULTIPLIER 	1000	// Number of times the light intensity PWM
  	 	 	 	 	 	 	 	 	 	 	 	// 	is faster than the stimulation frequency
 
 void receiveUARTpacket();
